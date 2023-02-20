b0e8690fec076d23c25e99cc986df208ad57cbec
tifitifititfitifiitifitit <71073045+tifitifititfitifiitifitit@users.noreply.github.com>
2023-02-21 01:42:43 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int l, p;
+	cin >> l >> p;
+
+	for (int i = 0; i < 5; i++) {
+		int tmp;
+		cin >> tmp;
+		cout << tmp - l * p << " ";
+	}
+}
