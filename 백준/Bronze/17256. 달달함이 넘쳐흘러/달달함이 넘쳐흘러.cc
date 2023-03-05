7fff67d02fae8a696634f9ed896fda2c868a8530
tifitifititfitifiitifitit <71073045+tifitifititfitifiitifitit@users.noreply.github.com>
2023-03-05 15:55:27 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    int d, e, f;
+    cin >> a >> b >> c;
+    cin >> d >> e >> f;
+    cout << d - c << " " << e / b << " " << f - a;
+}
